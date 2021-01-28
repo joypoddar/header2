6bbf57390dbb116e33823b02d91dbea6b6d5b02a
Joy Poddar <joypoddar.sd@gmail.com>
2021-01-28 20:35:53 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="antialiased bg-indigo-600">
+    <Navbar />
+    <HeroContent />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import HeroContent from "./components/HeroContent.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    HeroContent,
+  },
+};
+</script>
+
+<style>
+</style>
